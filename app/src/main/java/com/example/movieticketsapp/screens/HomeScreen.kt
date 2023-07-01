16e04197c42c4c1ae00a561af3df6236a576ddcc
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-07-01 21:24:23 +0200

    finish home Screen

--- app/src/main/java/com/example/movieticketsapp/screens/HomeScreen.kt
+++ app/src/main/java/com/example/movieticketsapp/screens/HomeScreen.kt
@@ -2,6 +2,7 @@ package com.example.movieticketsapp.screens
 
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,11 +11,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -30,6 +36,7 @@ import com.example.movieticketsapp.composables.SpacerVertical16
 import com.example.movieticketsapp.composables.SpacerVertical32
 import com.example.movieticketsapp.composables.Chip
 import com.example.movieticketsapp.composables.SpacerVertical8
+import com.example.movieticketsapp.ui.theme.PrimaryLight
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -37,54 +44,106 @@ fun HomeScreen() {
     val images = listOf(R.drawable.movie1, R.drawable.movie2, R.drawable.movie3)
     val pagerState = rememberPagerState(initialPage = 1) { images.size }
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        BlurImage(imageId = images[pagerState.currentPage])
-        Column(modifier = Modifier.fillMaxSize()) {
-            SpacerVertical32()
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Chip(title = "Now Showing", selected = true, OnClick = {} , textColor = Color.White)
-                SpacerHorizontal4()
-                Chip(title = "Coming Soon", selected = false, OnClick = {} , textColor = Color.White)
-            }
-            SpacerVertical16()
-            ImageSlider(imagesList = images, pagerState = pagerState)
-            SpacerVertical16()
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.baseline_access_time_24),
-                    contentDescription = "Time",
-                    tint = Color.Gray
-                )
-                SpacerHorizontal4()
-                Text(text = "2h 23m")
+    Scaffold(
+        bottomBar = {
+            BottomAppBar(containerColor = Color.Transparent) {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceAround,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.vector_1),
+                        contentDescription = "Home",
+                        tint = Color.White,
+                        modifier = Modifier
+                            .clip(CircleShape)
+                            .background(PrimaryLight)
+                            .padding(10.dp)
+                    )
 
+                    Icon(
+                        painter = painterResource(id = R.drawable.search_icon),
+                        tint = Color.Black,
+                        contentDescription = "search"
+                    )
+                    Icon(
+                        painter = painterResource(id = R.drawable.ticket_1),
+                        tint = Color.Black,
+                        contentDescription = "ticket"
+                    )
+                    Icon(
+                        painter = painterResource(id = R.drawable.user_icon),
+                        contentDescription = "person",
+                        tint = Color.Black
+                    )
+                }
             }
-            SpacerVertical16()
-            Text(
-                text = "Fantastic Beasts: The Secrets of Dumbledore",
-                modifier = Modifier.fillMaxWidth().padding(horizontal = 64.dp),
-                textAlign = TextAlign.Center,
-                fontWeight = FontWeight.Normal,
-                fontSize = 24.sp
-            )
-            SpacerVertical8()
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Chip(title = "Fantasy", selected = false)
-                SpacerHorizontal4()
-                Chip(title = "Adventure", selected = false)
+        }
+    ) { padding ->
+        Box(modifier = Modifier.fillMaxSize()) {
+            BlurImage(imageId = images[pagerState.currentPage])
+            Column(modifier = Modifier.fillMaxSize()) {
+                SpacerVertical32()
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Chip(
+                        title = "Now Showing",
+                        selected = true,
+                        OnClick = {},
+                        textColor = Color.White
+                    )
+                    SpacerHorizontal4()
+                    Chip(
+                        title = "Coming Soon",
+                        selected = false,
+                        OnClick = {},
+                        textColor = Color.White
+                    )
+                }
+                SpacerVertical16()
+                ImageSlider(imagesList = images, pagerState = pagerState)
+                SpacerVertical16()
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_access_time_24),
+                        contentDescription = "Time",
+                        tint = Color.Gray
+                    )
+                    SpacerHorizontal4()
+                    Text(text = "2h 23m")
+
+                }
+                SpacerVertical16()
+                Text(
+                    text = "Fantastic Beasts: The Secrets of Dumbledore",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 64.dp),
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 24.sp
+                )
+                SpacerVertical8()
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Chip(title = "Fantasy", selected = false)
+                    SpacerHorizontal4()
+                    Chip(title = "Adventure", selected = false)
+                }
             }
         }
     }
+
+
 }
 
 @Preview(showSystemUi = true)

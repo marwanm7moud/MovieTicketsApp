73ed20c16109b2a18a28a7284301c149c6c1b7f1
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-07-01 13:18:11 +0200

    add fonts

--- app/src/main/java/com/example/movieticketsapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/movieticketsapp/ui/theme/Type.kt
@@ -2,33 +2,29 @@ package com.example.movieticketsapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.movieticketsapp.R
 
 // Set of Material typography styles to start with
+val kumbhSans = FontFamily(
+    Font(R.font.kumbhsans_regular),
+    Font(R.font.kumbhsans_medium , FontWeight.Medium)
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = kumbhSans,
+        fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+    ,titleLarge = TextStyle(
+        fontFamily = kumbhSans,
+        fontWeight = FontWeight.Medium,
         fontSize = 22.sp,
         lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
